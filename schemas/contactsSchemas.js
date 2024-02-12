dbd2de126e935c7816355c8c7a29a5032f41e7c2
khrystyna-shekel <christina.shekel@gmail.com>
2024-02-12 17:24:35 +0200

    update v added

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -10,4 +10,6 @@ export const updateContactSchema = Joi.object({
   name: Joi.string().min(3),
   email: Joi.string().email(),
   phone: Joi.number(),
-});
+})
+  .min(1)
+  .message("Body must have at least one field");
